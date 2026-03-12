b49590a22e88c7d2c44a9a6a29293f2fe4c917cc
fsossa <fulbsossa16@gmail.com>
2026-03-12 13:40:59 +0100

    add specific function in dao casses

--- src/main/java/fr/istic/taa/jaxrs/dao/OrganizerDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/OrganizerDao.java
@@ -2,10 +2,21 @@ package fr.istic.taa.jaxrs.dao;
 
 import fr.istic.taa.jaxrs.dao.generic.AbstractJpaDao;
 import fr.istic.taa.jaxrs.domain.Organizer;
+import fr.istic.taa.jaxrs.domain.Concert;
+import java.util.List;
 
 public class OrganizerDao extends AbstractJpaDao <Long,Organizer>{
     public OrganizerDao() {
         super();
         this.setClazz(Organizer.class);
     }
+
+    public List<Concert> findConcertsByOrganizerId(Long organizerId) {
+        return this.entityManager
+                .createQuery("SELECT c FROM Concert c WHERE c.organizer.id = :organizerId", Concert.class)
+                .setParameter("organizerId", organizerId)
+                .getResultList();
+    }
+
+
 }
